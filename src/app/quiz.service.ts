5ceb7fbee76b5025948f75910fbf85c6a613904b
Jessica Flores <jafloresrgv@gmail.com>
2018-07-18 11:49:26 -0500

    resolved merge conflict and commited updated user results array and calculate results function

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -5,63 +5,68 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class QuizService {
-<<<<<<< Updated upstream
-  
-  constructor(private router: Router ) { 
+
+  constructor(private router: Router) {
     this.router = router;
   }
-  
+
   currentQuestion = 0;
-  result=0;
- 
-  finalResults= [
-    {
-      endScore: "Low", 
-      titleFinal: "title goes here",
-      image: "image here",
-      desc: "decription goes here"
-    },
-    {
-      endScore: "Middle", 
-      titleFinal: "title goes here",
-      image: "image here",
-      desc: "decription goes here"
-    },
-    {
-      endScore: "High", 
-      titleFinal: "title goes here",
-      image: "image here",
-      desc: "decription goes here"
-    }
-  ]
+  result: any;
+  userResults = [];
 
-// SUM of Tallies
+  finalResults =
+    [
+      {
+        endScore: 2,
+        endResult:
+        {
+          titleFinal: "1 title goes here",
+          image: "image here",
+          desc: "decription goes here"
+        },
+      },
+      {
+        endScore: 4,
+        endResult:
+        {
+          titleFinal: "2 title goes here",
+          image: "image here",
+          desc: "decription goes here"
+        },
+      },
+      {
+        endScore: 6,
+        endResult:
+        {
+          titleFinal: "3 title goes here",
+          image: "image here",
+          desc: "decription goes here"
+        }
+      }
+    ];
 
-  
-// msg = "";
+  // SUM of Tallies
 
-  recordAnswer(index){
- 
-alert(index);
- let p = this.questions[this.currentQuestion].choices[index].value;
- this.questions[this.currentQuestion].selectedAnswer = p;
+
+
+
+  recordAnswer(index) {
+
+    alert(index);
+    let p = this.questions[this.currentQuestion].choices[index].value;
+    this.questions[this.currentQuestion].selectedAnswer = p;
     alert(p);
     console.log(p);
-    
-    // let p = this.questions
-    // [this.currentQuestion].choices;
-   
-    // let y = this.questions[currentQuestion]
 
-    // p.forEach((e) => {
-    //   console.log(e.value)
-    //   userResult.push(e.value);
-    //   console.log("this is the new array");
-    //   console.log(userResult);
-    // });
-   
+    this.userResults.push(p);
 
-    if(this.questions.length -1 > this.currentQuestion) {
+    console.log(this.userResults);
+
+
+
+
+
+    if (this.questions.length - 1 > this.currentQuestion) {
       // console.log(p);
       this.currentQuestion++;
       this.router.navigate(['/question', this.currentQuestion]);
@@ -74,37 +79,65 @@ alert(index);
 
   }
 
-
-
-  caculateResult(){
-    //will generate results will use if & else or terinary in order to calculate result
-   
-    // if(this.result <= 10) {
-
-
-    //   this.finalResults.final;
-
-
-    // } else if(this.result > 10 && this.result <= 15) {
-
-    //   this.finalResults.final;
-
-
-    // } else if(this.result > 15 && this.result <= 20) {
-
-    //   this.finalResults.final;
-    // }
+  onClick(){
+    window.location.reload();
   }
 
 
 
-  
+  caculateResult() {
+    //will generate results will use if & else or terinary in order to calculate result
+    // this.userResults.reduce(function z(x,y){
+    //   return x + y;
+    // } );
+
+    this.result = this.userResults.reduce((x, y) => x + y, 0);
+
+    console.log(this.result);
+
+
+    if (this.result <= 10) {
+
+
+      this.result = this.finalResults[0];
+      console.log(this.result);
+
+
+    } else if (this.result > 10 && this.result <= 15) {
+
+
+      this.result = this.finalResults[1];
+      console.log(this.result);
+
+
+    } else if (this.result > 15 && this.result <= 20) {
+
+      this.result = this.finalResults[1];
+      console.log(this.result);
+
+    }
+  }
+
+  // restartQuiz(){
+  //   if (this.currentQuestion > 0) {
+  //     this.currentQuestion--;
+  //     //will go to previous question
+  //     this.router.navigate([`/question/${this.currentQuestion}`]);
+  //   } else {
+  //     //return home
+  //     this.router.navigate([`/`]);
+  //   }
+  // }
+
+
+
+
   questions = [
     {
       id: 1,
       title: "You are Captian of The Black Pearl, what is your first quest?",
       selectedAnswer: 0,
-      choices:[
+      choices: [
         {
           a: 'Sail the open Sea',
           value: 1
@@ -123,12 +156,12 @@ alert(index);
         }
       ],
     },
-    
+
     {
       id: 2,
       title: "The next question of the quiz goes here?",
       selectedAnswer: 0,
-      choices:[
+      choices: [
         {
           a: 'first answer goes here',
           value: 1
@@ -153,7 +186,7 @@ alert(index);
       id: 3,
       title: "The next question of the quiz goes here?2",
       selectedAnswer: 0,
-      choices:[
+      choices: [
         {
           a: 'first answer goes here2',
           value: 1
@@ -177,7 +210,7 @@ alert(index);
       id: 4,
       title: "The next question of the quiz goes here?3",
       selectedAnswer: 0,
-      choices:[
+      choices: [
         {
           a: 'first answer goes here3',
           value: 1
@@ -195,14 +228,14 @@ alert(index);
           value: 4
         }
       ],
-    
+
 
     },
     {
       id: 5,
       title: "The next question of the quiz goes here?",
       selectedAnswer: 0,
-      choices:[
+      choices: [
         {
           a: 'first answer goes here%',
           value: 1
@@ -222,18 +255,7 @@ alert(index);
       ],
     }
   ]
-  
-  }
+
+}
 
 
-
- // goPrevQuestion(){
-  //   if (this.currentQuestion > 0) {
-  //     this.currentQuestion--;
-  //     //will go to previous question
-  //     this.router.navigate([`/question/${this.currentQuestion}`]);
-  //   } else {
-  //     //return home
-  //     this.router.navigate([`/`]);
-  //   }
-  // }
